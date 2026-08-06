3de17e84b2a1a8723d50b79f43bf34550e2690f6
maciejwisniowski1-hash <maciej.wisniowski1@gmail.com>
2026-08-06 16:03:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -150,19 +150,29 @@ wyniki.append({
             })
 
 raport=pd.DataFrame(wyniki)
-st.dataframe(raport, use_container_width=True)
+
+st.dataframe(
+    raport, 
+    use_container_width=True
+)
+
 out=BytesIO()
+
 with pd.ExcelWriter(
     out, 
     engine='openpyxl'
 ) as writer:
-            raport.to_excel(
+    
+raport.to_excel(
                 writer, 
                 sheet_name='Wszystkie', 
                 index=False
             )
+
 out.seek(0)
+
 st.download_button('
 📥 Pobierz raport', 
 out, 
-'raport_faktur.xlsx')
+'raport_faktur.xlsx'
+)
